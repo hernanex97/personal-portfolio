2e9a0734256362804d8842958e55330ba19f7bda
hernanex97 <44402597+hernanex97@users.noreply.github.com>
2023-05-22 23:51:27 -0300

    Updates

---
+++ precache-manifest.6aad8e98797379f6e851af2a06cd2469.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "79f13792008d9a160a0b",
+    "url": "/personal-portfolio/static/css/main.e6e52734.chunk.css"
+  },
+  {
+    "revision": "79f13792008d9a160a0b",
+    "url": "/personal-portfolio/static/js/main.3d18f8ad.chunk.js"
+  },
+  {
+    "revision": "919df7781af4fef99075",
+    "url": "/personal-portfolio/static/js/runtime~main.e5e62f4e.js"
+  },
+  {
+    "revision": "7df29211884ddb258cd3",
+    "url": "/personal-portfolio/static/css/2.ca3c34b9.chunk.css"
+  },
+  {
+    "revision": "7df29211884ddb258cd3",
+    "url": "/personal-portfolio/static/js/2.bddfa12d.chunk.js"
+  },
+  {
+    "revision": "797739ac1b5698394f96",
+    "url": "/personal-portfolio/static/js/3.e339ce8c.chunk.js"
+  },
+  {
+    "revision": "dff2b6246274897c980cd8a354f7f979",
+    "url": "/personal-portfolio/static/media/nav-icon1.dff2b624.svg"
+  },
+  {
+    "revision": "922d0039dc6c25f3d94529d570e8d442",
+    "url": "/personal-portfolio/static/media/herno2.922d0039.png"
+  },
+  {
+    "revision": "a11263d7b07703d9a2d1a04774ba35e2",
+    "url": "/personal-portfolio/static/media/csharp_512x512.a11263d7.png"
+  },
+  {
+    "revision": "d53bca8384df266ee2036207ccf5f57a",
+    "url": "/personal-portfolio/static/media/python_512x512.d53bca83.png"
+  },
+  {
+    "revision": "89a17fb5b559f65d8014a252c437d741",
+    "url": "/personal-portfolio/static/media/javascript_512x512.89a17fb5.png"
+  },
+  {
+    "revision": "3769a06e4391e166bdd3fa83bbec3d66",
+    "url": "/personal-portfolio/static/media/css_logo.3769a06e.png"
+  },
+  {
+    "revision": "b496c56615af0f91a2696170ca62074b",
+    "url": "/personal-portfolio/static/media/html_white_512x512.b496c566.png"
+  },
+  {
+    "revision": "0a9018a7b798f72c757c6ae8a21a5beb",
+    "url": "/personal-portfolio/static/media/docker.0a9018a7.png"
+  },
+  {
+    "revision": "410c3e922fd2f5506c128072ad9ad8ca",
+    "url": "/personal-portfolio/static/media/k8s.410c3e92.png"
+  },
+  {
+    "revision": "ed508d9cd0c228ecaf90fa4f5b153d2a",
+    "url": "/personal-portfolio/static/media/unity_logo_512x512.ed508d9c.png"
+  },
+  {
+    "revision": "1fd3762a7d1a55e6bd4f51b86da9b2b3",
+    "url": "/personal-portfolio/static/media/firebase_logo2.1fd3762a.png"
+  },
+  {
+    "revision": "a84f23a89469daa1d58181191159efa2",
+    "url": "/personal-portfolio/static/media/sql_logo.a84f23a8.png"
+  },
+  {
+    "revision": "85e85996cd9a1f3fb5ef80ced35fed35",
+    "url": "/personal-portfolio/static/media/new-color-sharp.85e85996.png"
+  },
+  {
+    "revision": "c9b960470acfe8412d453802d58bef54",
+    "url": "/personal-portfolio/static/media/new-color-sharp2.c9b96047.png"
+  },
+  {
+    "revision": "17dbfa9bbee9eb71fc2e194e5572a047",
+    "url": "/personal-portfolio/static/media/fastcars.17dbfa9b.png"
+  },
+  {
+    "revision": "2dd9cc605f8a3be3dff06e198206bf76",
+    "url": "/personal-portfolio/static/media/officehero.2dd9cc60.png"
+  },
+  {
+    "revision": "8ef6872d74e2c612870b39931afe02f6",
+    "url": "/personal-portfolio/static/media/leagofgoals.8ef6872d.png"
+  },
+  {
+    "revision": "48fb25e9b7f8bf1a86974be7c3478b76",
+    "url": "/personal-portfolio/static/media/foresthope.48fb25e9.png"
+  },
+  {
+    "revision": "cfec02e3119564aa5d30c6df3a35e3b7",
+    "url": "/personal-portfolio/static/media/electrons.cfec02e3.png"
+  },
+  {
+    "revision": "e1a857460aaea6f08ff8d0f1f66dbd20",
+    "url": "/personal-portfolio/static/media/watchviewer.e1a85746.png"
+  },
+  {
+    "revision": "d0ac5460d6b0114d70ef726b98e36a2e",
+    "url": "/personal-portfolio/static/media/uif.d0ac5460.png"
+  },
+  {
+    "revision": "a8822e204211be92b2412ccbe284e785",
+    "url": "/personal-portfolio/static/media/contact-img.a8822e20.svg"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/personal-portfolio/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/personal-portfolio/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/personal-portfolio/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "45625b8b3c5fbb1734f8b5cbb3ed0981",
+    "url": "/personal-portfolio/static/media/new-footer-bg.45625b8b.png"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/personal-portfolio/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/personal-portfolio/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/personal-portfolio/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "daea9d9ce6c3477441655a4b3c90774d",
+    "url": "/personal-portfolio/index.html"
+  }
+];
